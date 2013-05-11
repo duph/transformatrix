567ef9fbd25c9ee74c1d57600552e59cb3fc2346
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-05-11 15:25:19 -0700

    rotation, translation, reflection, scaling implemented

--- lib/transformatrix.rb
+++ lib/transformatrix.rb
@@ -10,12 +10,33 @@ class TMatrix
 		self
 	end
 
-	def rotation angle
-		base = []
-		@matrix = Matrix[[cos(angle), sin(angle), 0], [-sin(angle), cos(angle), 0], [0, 0, 1]]
-
+	def [] param
+		@matrix = Matrix[param]
+		self
 	end
 
-	def tranlation x, y
+	#Create matrix for rotating alpha radians about origo
+	def rotation alpha
+		@matrix = Matrix[[cos(alpha), sin(alpha), 0], [-sin(alpha), cos(alpha), 0], [0, 0, 1]]
+		self
+	end
+
+	#Create matrix for translating by [tx, ty]
+	def tranlation tx, ty
+		@matrix = Matrix[[1, 0, tx], [0, 1, ty], [0, 0, 1]]
+		self
+	end
+
+	#Create matrix for reflecting about a line interpolating (0,0) and (lx,ly)
+	#b=lx a=-ly
+	def reflection lx, ly
+		@matrix = Matrix[[lx**2-ly**2, 2*lx*ly, 0], [2*lx*ly, ly**2-lx**2, 0], [0, 0, lx**2+ly**2]]
+		self
+	end
+
+	#Create matrix for scaling relative to origo
+	def scaling sx, sy
+		@matrix = Matrix[[sx, 0, 0], [sy, 0, 0], [0, 0, 1]]
+		self
 	end
 end
